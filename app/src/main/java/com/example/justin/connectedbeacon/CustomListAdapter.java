2bf86a59ddc5ad80432c9b8a9ea28490c047b530
justin <justinmnl@hotmail.com>
2017-05-26 14:13:32 +0200

    code
    commited on 26,5,2017

---
+++ app/src/main/java/com/example/justin/connectedbeacon/CustomListAdapter.java
@@ -0,0 +1,44 @@
+package com.example.justin.connectedbeacon;
+
+/**
+ * Created by Justin on 5/14/2017.
+ */
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    private final Integer[] imgid;
+
+    public CustomListAdapter(Activity context, String[] itemname, Integer[] imgid) {
+        super(context, R.layout.customlayout, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.customlayout, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.textView_name);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView);
+        TextView extratxt = (TextView) rowView.findViewById(R.id.textView_description);
+
+        txtTitle.setText(itemname[position]);
+        imageView.setImageResource(imgid[position]);
+        extratxt.setText("Description "+itemname[position]);
+        return rowView;
+
+    };
+}
---
+++ app/src/main/java/com/example/justin/connectedbeacon/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.justin.connectedbeacon;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+
+public class MainActivity extends Activity implements BeaconConsumer {
+    public static final String TAG = "BeaconsEverywhere";
+    ListView list;
+    private BeaconManager beaconManager;
+    String[] itemname ={
+            "Kiln",
+            "Pump",
+            "Conveyor",
+
+
+    };
+
+    Integer[] imgid={
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        beaconManager.bind(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        final Region region = new Region("myBeacons", Identifier.parse("F001889B-7509-4C31-A905-1A27039C003C"), null, null);
+
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                try {
+                    Log.d(TAG, "didEnterRegion");
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                try {
+                    Log.d(TAG, "didExitRegion");
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for(Beacon oneBeacon : beacons) {
+                    Log.d(TAG, "distance: " + oneBeacon.getDistance() + " id:" + oneBeacon.getId1() + "/" + oneBeacon.getId2() + "/" + oneBeacon.getId3());
+
+
+                    Toast.makeText(getBaseContext(),Double.toString( oneBeacon.getDistance() ), Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+
+
+//        try {
+//            beaconManager.startMonitoringBeaconsInRegion(region);
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+
+}
